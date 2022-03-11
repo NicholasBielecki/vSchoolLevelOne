3ec4c9884d4b0362dc6d06134939b2df7aa0fb9d
Nicholas Bielecki <nicholas.j.bielecki@gmail.com>
2022-03-11 14:47:19 -0800

    031122 commit

--- colossalAdventure/main.js
+++ colossalAdventure/main.js
@@ -1,18 +1,17 @@
-//KNOWN BUGS 1. Sometimes player health and enemy health both hit 0 at same time. This results in console logging that you killed it then died. This is due to the while loop having to make a complete cycle. 
+
 
 var readlineSync = require('readline-sync')
 //walking
 let walkCounter = 40
 let randomNumber3 = 0
-let randomNumber4 = 0
 let startStop = true
 //game stuff
 let enemies = ["COVID VIRUS", "U.S Dollar Inflation", "Vladimir Putin"]
-let playerHealth = 5
+let playerHealth = 500
 let isALIVE = true
 let playerInventory = []
 
-
+//player and intro
 const name = readlineSync.question("Hello, what is your name?")
 let instructions = readlineSync.keyIn("Hello " + name + " Press w to start walking, walk 10 times to win:")
 
@@ -23,9 +22,11 @@ while(walkCounter>0 && startStop === true && isALIVE === true ){
 
 //Random Counter 3
 function randomCounter3(){
-    randomNumber3 =  Math.floor(Math.random() *4)
+    randomNumber3 =  Math.floor(Math.random() *4)  
 }
+
 randomCounter3()
+
 if(randomNumber3 === 3){    
     startStop = false
     beingAttacked = true
@@ -44,8 +45,11 @@ while(beingAttacked === true && isALIVE === true){
         //failed run         
                  if(runChance===1){
                      playerHealth--
-                     console.log("You did not escape. You lost one health state and have:" + playerHealth + " health. You must now fight the wild " + enemy )
-                     if(playerHealth <=0){
+                     if(playerHealth>0){
+                        console.log("You did not escape. You lost one health state and have:" + playerHealth + " health. You must now fight the wild " + enemy )
+                     }
+                     
+                     else{
                          beingAttacked=false
                          isALIVE=false
                          console.log("You died")
@@ -74,10 +78,13 @@ while(beingAttacked === true && isALIVE === true){
                  if(warning === "ATTACK"){                     
                    while(playerHealth>0 && enemyHealth>0){
                         playerHealth -= Math.floor(Math.random() *3)
-                        enemyHealth -= Math.floor(Math.random() *3)
+                       
+                        if(playerHealth > 0){
+                            enemyHealth -= Math.floor(Math.random() *3)
+                        }                        
                             if(enemyHealth <= 0){
                                 playerInventory.push(1)
-                                console.log("You killed it and now have:" + playerHealth + " health and have been awarded a potion. You now have" + playerInventory.length + " potions")
+                                console.log("You killed it and now have:" + playerHealth + " health and have been awarded a potion. You now have " + playerInventory.length + " potions")
                                 beingAttacked = false
                             }
                             if(playerHealth <= 0){
@@ -90,22 +97,23 @@ while(beingAttacked === true && isALIVE === true){
                 
 }
 //potion time
-    if(startStop === false && isALIVE === true){
-        let instructions = readlineSync.keyIn("Press w to continue walking or press p to use a potion and restore 1 health. You have" + playerInventory.length + " potions left")
+    while(startStop === false && isALIVE === true){
+        let instructions = readlineSync.keyIn("Press w to continue walking or press p to use a potion and restore 1 health. You have " + playerInventory.length + " potions left")
         if(instructions === "w"){
             startStop = true
         }
-        if(instructions === "p"){
-            if(playerInventory<=0){
+        if(instructions === "p" && playerInventory <= 0){
+            
                 console.log("You dont have anymore potions and must continue walking")
                 startStop=true
             }
-            else{
+            if(instructions === "p" && startStop === false){
                 playerInventory.pop()
                 playerHealth ++
-                console.log("Your health is now " + playerHealth + " and you have " + playerInventory + " potions left")
-                startStop=true
-            }
+                console.log("Your health is now " + playerHealth + " and you have " + playerInventory.length + " potions left")
+                instructions
+                
+            
         }
     }
     
--- fizzBuzz/main.js
+++ fizzBuzz/main.js
@@ -2,20 +2,20 @@ let numbers = []
 let counter = {fizz: 0, buzz:0, fizzbuzz:0 }
 
 fizz = function(){
-    for(i=1;i<100;i++){
+    for(i=0;i<100;i++){
         numbers.push(i)
 
-       if(i % 3 === 0){
+       if(i % 3 === 0 && i > 0){
             numbers.splice(i,1,"fizz")
             counter.fizz++
        }
 
-       if(i % 5 ===0){
+       if(i % 5 ===0 && i > 0){
            numbers.splice(i,1,"buzz")
             counter.buzz++
        }
 
-       if(i % 3 === 0 && i % 5 === 0){
+       if(i % 3 === 0 && i % 5 === 0 && i > 0){
            numbers.splice(i,1,"fizzbuzz")
            counter.fizzbuzz++
        }
